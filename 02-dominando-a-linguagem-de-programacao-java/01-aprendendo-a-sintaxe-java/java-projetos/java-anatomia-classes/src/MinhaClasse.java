67a65d2719b70c3d4c08b11053af22a0200194e5
Felipe Dantas do Nascimento <56466463+Felipeddn@users.noreply.github.com>
2024-07-19 21:58:47 -0300

    Exemplos MinhaClasse.java na aula anatomia 3

--- 02-dominando-a-linguagem-de-programacao-java/01-aprendendo-a-sintaxe-java/java-projetos/java-anatomia-classes/src/MinhaClasse.java
+++ 02-dominando-a-linguagem-de-programacao-java/01-aprendendo-a-sintaxe-java/java-projetos/java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,31 @@
 public class MinhaClasse {
    //bloco de código
     public static void main(String [] args){
-        System.out.print("Primeira aula sobre anatomia das classes.");
+        //System.out.print("Primeira aula sobre anatomia das classes.");
+
+       //sobre variáveis 
+       /*
+       String meuNome = "Felipe";
+
+       int anoFabricacao = 2022;
+
+       boolean verdadeira = true;
+       */
+
+       //podemos manipular o valor de uma variável após uma valor ser atribuido a mesma;
+       //anoFabricacao = 2018;
+
+       //sobre métodos
+       String primeiroNome = "Felipe";
+       String segundoNome = "Dantas"
+
+       String nomeCompleto = nomeCompleto (primeiroNome, segundoNome)
+
+      System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+   return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 }
 
 }
