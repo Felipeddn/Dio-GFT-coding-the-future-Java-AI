984165b12747244c6065d65b1c8964211742a36d
Felipe Dantas do Nascimento <56466463+Felipeddn@users.noreply.github.com>
2024-07-25 11:20:50 -0300

    feat: fim dos exemplos da aula 7  Operadores.java relacionais

--- 02-dominando-a-linguagem-de-programacao-java/01-aprendendo-a-sintaxe-java/java-projetos/02-tipos-e-variaveis/02-tipos-e-variaveis-projeto-java/src/Operadores.java
+++ 02-dominando-a-linguagem-de-programacao-java/01-aprendendo-a-sintaxe-java/java-projetos/02-tipos-e-variaveis/02-tipos-e-variaveis-projeto-java/src/Operadores.java
@@ -186,10 +186,26 @@ public class Operadores {
     boolean condicao1 = true;
     boolean condicao2 = false;
 
+    /*
     if (condicao1 && condicao2){
       System.out.println("As duas condições são verdadeiras");
     }
     System.out.println("Fim");
+    */
+
+    if (condicao1 || condicao2){
+      System.out.println("Uma das condições condições é verdadeiras");
+    }
+
+    condicao2 = true;
+
+    if (condicao1 && condicao2){
+      System.out.println("As duas condições são verdadeiras");
+    }
+
+    if (condicao1 && (7 > 4)){
+      System.out.println("As duas condições são verdadeiras");
+    }
 
   }
 }
